3a6d111c6ac284bf6e8b06299f35f24bb085bd8f
ghassan <ghassan>
2016-03-18 00:36:23 +0200

    included the code for the cache inverse matrix
    it includes the code to test the functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+#Barmil Tanak
+#18 Mar 2016
+#Programming Assignemnmt 2 Week 3
 
-## Write a short comment describing this function
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x =matrix())  {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinversematrix <- function(inversematrix) m <<- inversematrix
+  getinversematrix <- function() m
+  list(set = set, get = get,
+       setinversematrix = setinversematrix,
+       getinversematrix = getinversematrix)
 }
-
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinversematrix()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m<- solve(data, ...)
+  x$setinversematrix(m)
+  m
 }
+
+#The below Code is to test the tabove functions:
+
+message("Create an invertible Matrix")
+myMat<- matrix(c(-1,-2,-2,-1,2,0,2,-1,4),nrow=3, ncol=3,byrow=TRUE, dimnames = list(c("R1","R2","R3"),c("C1","C2","C3")))
+myMat
+message("Test if it is invertible")
+solve(myMat)
+message("Create the Special Matrix")
+specialmat<-makeCacheMatrix()
+specialmat$set(myMat)
+message("Compute the inverse 1st time: no Cache It should compute the inverse")
+cacheSolve(specialmat)
+message("Compute the inverse 2nd time: Inverse retreived from the Cache")
+cacheSolve(specialmat)
